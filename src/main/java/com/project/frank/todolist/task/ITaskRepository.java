e0a66e8899ea400fe537c434b6a0a01785f0b3f6
frank-cardoso <frank.cardoso032@gmail.com>
2023-11-05 13:38:11 -0300

    Dockerfile Deploy

---
+++ src/main/java/com/project/frank/todolist/task/ITaskRepository.java
@@ -0,0 +1,15 @@
+
+package com.project.frank.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.config.Task;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+   List<TaskModel> findByIdUser(UUID idUser);
+
+
+}
